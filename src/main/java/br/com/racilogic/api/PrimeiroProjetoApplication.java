47cc793a2a71d3cd297b760ad498b2b869760f55
racilogic <racilogic@gmail.com>
2018-11-09 03:27:46 -0200

    Alteração no arq principal da aplicação

--- src/main/java/br/com/racilogic/api/PrimeiroProjetoApplication.java
+++ src/main/java/br/com/racilogic/api/PrimeiroProjetoApplication.java
@@ -8,5 +8,6 @@ public class PrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroProjetoApplication.class, args);
+		System.out.println("Meu primeiro projeto...");
 	}
 }
