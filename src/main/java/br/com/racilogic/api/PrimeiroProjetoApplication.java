6db4736a8c3af9908a133aa668c2563846383277
racilogic <racilogic@gmail.com>
2018-11-09 03:11:06 -0200

    Versão inicial do projeto

---
+++ src/main/java/br/com/racilogic/api/PrimeiroProjetoApplication.java
@@ -0,0 +1,12 @@
+package br.com.racilogic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoApplication.class, args);
+	}
+}
